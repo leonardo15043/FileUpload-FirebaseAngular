d2fab868a99e21767f464bea60ea1aabcfb81916
leonardo15043 <lahernandez15043@gmail.com>
2019-02-01 22:59:43 -0500

    Mostrar listado de imágenes subidas

--- src/app/services/carga-imagenes.service.ts
+++ src/app/services/carga-imagenes.service.ts
@@ -17,13 +17,44 @@ export class CargaImagenesService {
     ) { }
 
     cargarImagenesFirebase( imagenes: FileItemsModule[] ) {
-       console.log( imagenes );
+
+       const storageRef = firebase.storage().ref();
+
+       for ( const item of imagenes) {
+
+        item.estaSubiendo = true;
+        if ( item.progreso >= 100 ) {
+            continue;
+        }
+
+        const referenciaImagen = storageRef.child( `${ this.CARPETA_IMAGENES }/${ item.nombreArchivo }` );
+        const uploadTask: firebase.storage.UploadTask = referenciaImagen.put( item.archivo );
+
+        uploadTask.on( firebase.storage.TaskEvent.STATE_CHANGED ,
+              ( snapshot: firebase.storage.UploadTaskSnapshot ) =>
+                           item.progreso = ( snapshot.bytesTransferred / snapshot.totalBytes ) * 100 ,
+              ( error ) => console.error('Error al subir', error),
+              () => {
+                referenciaImagen.getDownloadURL().then(
+                    ( urlImagen ) => {
+                        console.log('Imagen cargada correctamente');
+                        item.url = urlImagen;
+                        item.estaSubiendo = false;
+                        this.guardarImagen({
+                            nombre: item.nombreArchivo,
+                            url: item.url
+                        });
+                    },
+                    ( error ) => console.log('No existe la URL')
+                );
+            });
+
+       }
     }
 
-
-    // private guardarImagen( imagen: { nombre: string , url: string }) {
-    //   this.db.collection(`/${ this.CARPETA_IMAGENES }`)
-    //       .add( imagen );
-    // }
+    private guardarImagen( imagen: { nombre: string , url: any } ) {
+      this.db.collection(`/${ this.CARPETA_IMAGENES }`)
+          .add( imagen );
+    }
 
 }
